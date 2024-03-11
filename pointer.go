8438a420da12c21018842d0ba63a4da06e85eb24
Plabon Kumar saha <46686524+PlabonKumarsaha@users.noreply.github.com>
2024-03-11 15:00:20 +0600

    Create pointer.go

---
+++ pointer.go
@@ -0,0 +1,16 @@
+package main
+import ("fmt","sync")
+
+
+var wg = sync.WaitGroup{}
+var dbData = []string {"id1","id2","id3"}
+
+func main(){
+	t0 := time.Now()
+ var p *int32 = new(int32)
+
+ var i int32
+ fmt.printf("The value is : %v",*p)
+ fmt.printf("\nThe value is : %v",i)
+
+}
